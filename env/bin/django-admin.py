348c1144d14d1d0fdd518ffb5dc248552bcd2eba
victor <victorkrook96@gmail.com>
2020-03-27 18:35:36 +0100

    gitignore

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/victor/skola/kurser/digitala_system/projekt_eita15/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
